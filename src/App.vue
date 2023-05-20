2d5477b44d6db9d4032999b3b276146fec9dda13
Rajat <rajatky3107@gmail.com>
2023-05-20 10:39:39 +0530

    custom component events

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,35 @@
 <script>
 import Greet from "./components/Greet.vue";
 import Articless from "./components/Article.vue";
+import Popup from "./components/Popup.vue";
 export default {
   name: "App",
   components: {
     Greet,
     Articless,
+    Popup,
   },
   data() {
     return {
-      name: "Musafir",
-      channel: "Aawara",
+      showPopUp: false,
     };
   },
-  methods: {},
+  methods: {
+    closePopup(name) {
+      this.showPopUp = false;
+      // alert(name);
+      console.log(name);
+    },
+  },
   computed: {},
   watch: {},
 };
 </script>
 
 <template>
-  <Greet name="Rajat" />
-  <Greet name="Rajesh" />
-  <Greet name="Ramesh" />
-  <Greet :name="name" />
-  <!-- Need vbinding for other than string values       -->
-  <Articless id="test-id" title="hah" :score="50" />
+  <button v-show="!showPopUp" @click="showPopUp = true">Show popup</button>
+  <!-- <Popup v-show="showPopUp" @close="showPopUp = false"></Popup> -->
+  <Popup v-show="showPopUp" @close="closePopup"></Popup>
 </template>
 
 <style scoped>
