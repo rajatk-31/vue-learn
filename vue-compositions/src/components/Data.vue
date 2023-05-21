0d6ace2c9f90749330ab1abf39fcabb4a3fafe2d
Rajat <rajatky3107@gmail.com>
2023-05-21 11:13:05 +0530

    Replacing data w reactive (data  use reactive)

--- vue-compositions/src/components/Data.vue
+++ vue-compositions/src/components/Data.vue
@@ -1,21 +1,41 @@
 <template>
   <h2>Options - {{ o_firstName }}</h2>
   <h2>Composition - {{ c_firstName }}</h2>
+
+  <h2>Composition {{ greet }}</h2>
+
+  <h2>Reactive {{ reactiveGreet }}</h2>
 </template>
 
 <script>
-import {} from "vue";
+import { ref, reactive } from "vue";
 export default {
   name: "Data",
   setup() {
-    const c_firstName = ref("Kumar");
+    const c_firstName = ref("Ramesh");
+    const c_lastName = ref("Yadav");
+    const c_heroName = ref("Aawara");
+    const greet = `Hello ${c_firstName.value} ${c_lastName.value} a.k.a. ${c_heroName.value}`;
+    const state = reactive({
+      firstName: "Princess",
+      lastName: "Diana",
+      heroName: "Wonder Woman",
+    });
+    const reactiveGreet = ` Hello ${state.firstName}`;
     return {
       c_firstName,
+      c_lastName,
+      c_heroName,
+      state,
+      reactiveGreet,
+      greet,
     };
   },
   data() {
     return {
       o_firstName: "Rajat",
+      o_lastName: "Kumar",
+      o_heroName: "Musafir",
     };
   },
 };
