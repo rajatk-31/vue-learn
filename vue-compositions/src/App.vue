30ba71d9dbedf1ba795a975853160f5515e231a6
Rajat <rajatky3107@gmail.com>
2023-05-21 10:54:00 +0530

    usinf ref in composition to replace data

--- vue-compositions/src/App.vue
+++ vue-compositions/src/App.vue
@@ -1,17 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Data from "./components/Data.vue";
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <Data></Data>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
---
+++ vue-compositions/src/components/Data.vue
@@ -0,0 +1,24 @@
+<template>
+  <h2>Options - {{ o_firstName }}</h2>
+  <h2>Composition - {{ c_firstName }}</h2>
+</template>
+
+<script>
+import {} from "vue";
+export default {
+  name: "Data",
+  setup() {
+    const c_firstName = ref("Kumar");
+    return {
+      c_firstName,
+    };
+  },
+  data() {
+    return {
+      o_firstName: "Rajat",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
